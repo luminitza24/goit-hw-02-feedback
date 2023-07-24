63beb9e2a3bf0a72d20da6c7cb2d050cc03f530a
luminitza24 <cluminita24@yahoo.com>
2023-07-24 22:36:34 +0300

    modificat proiect

--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,62 +1,33 @@
-import { Component } from 'react';
-import css from '../feedback/Feedback.css';
+import './Feedback.css';
 
-export class Feedback extends Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      bad: 0,
-      neutral: 0,
-    };
+const Feedback = ({ addFeedback }) => {
+  const data = [
+    { id: 1, value: 'good', text: 'Good' },
+    { id: 2, value: 'neutral', text: 'Neutral' },
+    { id: 3, value: 'bad', text: 'Bad' },
+  ];
 
-    handleIncrement = () => {
-      this.setState((state, props) => ({
-        value: state.value + props.step,
-      }));
-    };
-    handleClickGood = () => {
-      this.setState(state => ({
-        good: this.state.good + 1,
-      }));
-    };
-    this.handleClickBad = () => {
-      this.setState(state => ({
-        bad: this.state.bad + 1,
-      }));
-    };
-    this.handleClickNeutral = () => {
-      this.setState(state => ({
-        neutral: this.state.neutral + 1,
-      }));
-    };
-  }
-  render() {
-    return (
-      <div className="feedback">
-        <h2 class="title">Please leave feedback!</h2>
-        <button
-          className="buttons"
-          type="button"
-          onClick={this.handleClickGood.bind(this)}
-        >
-          Good: {good}
-        </button>
-        <button
-          className="buttons"
-          type="button"
-          onClick={this.handleClickNeutral.bind(this)}
-        >
-          Neutral:{neutral}
-        </button>
-        <button
-          className="buttons"
-          type="button"
-          onClick={this.handleClickBad.bind(this)}
-        >
-          Bad:{bad}
-        </button>
-      </div>
-    );
-  }
-}
+  const handleClick = e => {
+    const value = e.target.value;
+    addFeedback(value);
+  };
+  return (
+    <div className="buttons-section">
+      {data.map(btn => {
+        const { id, value, text } = btn;
+        return (
+          <button
+            key={id}
+            className="buttons"
+            value={value}
+            onClick={handleClick}
+          >
+            {text}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Feedback;
--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,25 +1,35 @@
-import { Component } from 'react';
+import '../statistics/Statistics.css';
 
-export class Statistics extends Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      bad: 0,
-      neutral: 0,
-      percentage: 0,
-      total: 0,
-    };
-  }
-  countTotal = () => {
-    this.setState.total =
-      this.setState.good + this.setState.bad + this.setState.neutral;
-    return { total };
+const Statistics = ({ good, neutral, bad, countFeedback }) => {
+  const countPercentage = () => {
+    const percentage = Math.round((good / countFeedback) * 100);
+    return percentage || 0;
   };
-  countPercentage = () => {
-    this.setState.percentage = Math.round(
-      (this.setState.good / this.setState.total) * 100
-    );
-    return { percentage } || 0;
-  };
-}
+  const data = [
+    { id: 1, text: 'Good: ', value: good },
+    { id: 2, text: 'Neutral: ', value: neutral },
+    { id: 3, text: 'Bad: ', value: bad },
+    { id: 4, text: 'Total: ', value: countFeedback },
+    {
+      id: 5,
+      text: 'Positive feedback: ',
+      value: countPercentage(),
+      percentage: '%',
+    },
+  ];
+
+  return (
+    <div className="statistics-section">
+      {data.map(item => {
+        return (
+          <p className="statistics" key={item.id}>
+            {item.text}
+            {item.value}
+            {item.percentage}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+export default Statistics;
